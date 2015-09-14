f476843a69125400967bb012b613c6cee2c5acf5
ee7klt <kltah1@gmail.com>
2015-09-14 12:34:28 +0800

    Newton's method for finding square roots. how to include if statements in def?

---
+++ eclipse/HelloWorld/.worksheet/src/greeter.newton.scala
@@ -0,0 +1,6 @@
+package greeter
+
+object newton {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(38); val res$0 = 
+  1+2;System.out.println("""res0: Int(3) = """ + $show(res$0));$skip(42); 
+  def abs(x: Double) = if (x<0) -x else x;System.out.println("""abs: (x: Double)Double""")}
+}
---
+++ eclipse/HelloWorld/src/greeter/hello.scala
@@ -0,0 +1,10 @@
+package greeter
+
+/**
+ * @author macgeekalex
+ */
+object Hello extends App {
+
+println("Hello, World!")
+
+}
