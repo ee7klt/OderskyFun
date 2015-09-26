0b11b106c506cb548ca406c2c1e7c78b16ea10fa
ee7klt <kltah1@gmail.com>
2015-09-26 09:59:32 +0800

    pascal working now, removed the for loop. but how to throw exception when c > r only on the first pass? (my iterations may have c>r which i need to let pass through)

--- eclipse/funprog/.worksheet/src/week1.assignment1.scala
+++ eclipse/funprog/.worksheet/src/week1.assignment1.scala
@@ -1,16 +1,20 @@
 package week1
 
 object assignment1 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(79); 
-  println("Welcome to the Scala worksheet");$skip(213); 
+  println("Welcome to the Scala worksheet");$skip(222); 
 
   def pascal(c: Int, r: Int): Int = {
-    if (c == 0 && r == 0) 1 else {
-      for (col <- 0 to 1) {
-        if (col == 0 || col == c) 1 else { pascal(c - 1, r - 1) + pascal(c, r - 1) }
-      }
-      0
-    }
-  };System.out.println("""pascal: (c: Int, r: Int)Int""");$skip(16); val res$0 = 
+  
 
-  pascal(2, 1);System.out.println("""res0: Int = """ + $show(res$0))}
+    if (c == 0 && r == 0) 1 else {
+        if (c == 0) 1 else {
+        	if (r == 0) 0 else {
+        	  pascal(c - 1, r - 1) + pascal(c, r - 1)
+        	  }
+        	}
+    }
+  };System.out.println("""pascal: (c: Int, r: Int)Int""");$skip(19); val res$0 = 
+  
+
+  pascal(2, 3);System.out.println("""res0: Int = """ + $show(res$0))}
 }
