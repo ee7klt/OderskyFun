52d829a1acb0934e9876879fd4936f53f747a084
ee7klt <kltah1@gmail.com>
2015-09-28 01:23:08 +0800

    throw runteimexception if c>r

--- eclipse/funprog/.worksheet/src/week1.assignment1.scala
+++ eclipse/funprog/.worksheet/src/week1.assignment1.scala
@@ -1,20 +1,24 @@
 package week1
 
 object assignment1 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(79); 
-  println("Welcome to the Scala worksheet");$skip(222); 
+  println("Welcome to the Scala worksheet");$skip(350); 
 
   def pascal(c: Int, r: Int): Int = {
-  
 
-    if (c == 0 && r == 0) 1 else {
+    def helper(c: Int, r: Int): Int = {
+      if (c == 0 && r == 0) 1 else {
         if (c == 0) 1 else {
-        	if (r == 0) 0 else {
-        	  pascal(c - 1, r - 1) + pascal(c, r - 1)
-        	  }
-        	}
+          if (r == 0) 0 else {
+            helper(c - 1, r - 1) + helper(c, r - 1)
+          }
+        }
+      }
     }
+    if (c>r) throw new RuntimeException("c must be <= r") else  helper(c,r)
+   
+
   };System.out.println("""pascal: (c: Int, r: Int)Int""");$skip(19); val res$0 = 
   
 
-  pascal(2, 3);System.out.println("""res0: Int = """ + $show(res$0))}
+  pascal(2, 4);System.out.println("""res0: Int = """ + $show(res$0))}
 }
