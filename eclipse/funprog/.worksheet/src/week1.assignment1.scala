b8f3956fb5b20c576a70e356bd22a11cffdb9f65
ee7klt <kltah1@gmail.com>
2015-10-05 14:03:31 +0800

    Assignment 1: counting change using recursive process

--- eclipse/funprog/.worksheet/src/week1.assignment1.scala
+++ eclipse/funprog/.worksheet/src/week1.assignment1.scala
@@ -125,6 +125,18 @@ object assignment1 {;import org.scalaide.worksheet.runtime.library.WorksheetSupp
   	check(0,chars) == 0
   };System.out.println("""balance4: (chars: List[Char])Boolean""");$skip(89); val res$5 = 
   
-  balance4("I told him (that it’s not (yet) done). (But he wasn’t listening)".toList);System.out.println("""res5: Boolean = """ + $show(res$5))}
+  balance4("I told him (that it’s not (yet) done). (But he wasn’t listening)".toList);System.out.println("""res5: Boolean = """ + $show(res$5));$skip(379); 
+                                                  
+                                                  
+                                                  
+    def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money < 0) 0
+    else if (coins.length == 0) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  };System.out.println("""countChange: (money: Int, coins: List[Int])Int""");$skip(46); val res$6 = 
+  
+  countChange(100, List(1, 5, 10, 25, 50));System.out.println("""res6: Int = """ + $show(res$6));$skip(48); val res$7 = 
+  countChange(300,List(5,10,20,50,100,200,500));System.out.println("""res7: Int = """ + $show(res$7))}
   
 }
